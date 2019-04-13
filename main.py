26395b6d6fb27f5f55c3854d81500be9be6fd276
MARC <melliott22@qub.ac.uk>
2019-04-13 17:39:19 +0100

    Added universal root

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import csv
 
 reviewLines = list()
 
-f = open("C:\\Users\Marc\\Desktop\\realReview", encoding='utf8')
+f = open(".\\dataset\\real_reviews.txt", encoding='utf8')
 #print(f.readline())
 
 for line in f :
@@ -16,16 +16,3 @@ for line in f :
 for item in reviewLines:
     print(item)
 
-#import and view dataset
-#git clone https://github.com/TukeBean/SpoonsHackerz.git
-#kickstarter = pd.read_csv("SpoonsHackerz/blob/master/dataset/fake_reviews.txt")
-#print(kickstarter.shape)
-#kickstarter.head(10)
-#
-#https://github.com/TukeBean/SpoonsHackerz/blob/master/dataset/fake_reviews.txt
-#
-##import and view dataset
-#!git clone https://github.com/Jake-Young/AI-NI-Academy.git
-#kickstarter = pd.read_csv("/content/AI-NI-Academy/AI NI Data Workshop/kickstarter2018.csv")
-#print(kickstarter.shape)
-#kickstarter.head(10)
